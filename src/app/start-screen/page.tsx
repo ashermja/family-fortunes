c763cf3be383594a590b78c411633a4917f7266b
Marc Asher <marcasher@hotmail.com>
2024-04-03 22:03:43 +0200

    Added a start screen

---
+++ src/app/start-screen/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+import bg from "../assets/family-fortunes.gif";
+import Link from "next/link";
+
+export default function StartScreen() {
+  const router = useRouter();
+
+  return (
+    <main className={styles.main}>
+      <div
+        style={{
+          backgroundImage: `url(${bg.src})`,
+          height: "100vh",
+          backgroundSize: "cover",
+        }}
+      >
+        <Link className={styles.start} href={`/`}>
+          START
+        </Link>
+      </div>
+    </main>
+  );
+}
